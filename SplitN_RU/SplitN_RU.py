feab4b4dc3c7e6ea687709cb257531b5c8fb2b7a
Ruwaa-Ib <ruwaa.ibrahem@gmail.com>
2019-05-04 23:30:16 +0200

    final commit isA

--- SplitN_RU/SplitN_RU.py
+++ SplitN_RU/SplitN_RU.py
@@ -1,80 +1,107 @@
 # importing libraries
 import pysam
 import re
+import argparse
+
+args = None
+
+# ----------------------------------------------------------------------
+def get_args():
+	""""""
+	parser = argparse.ArgumentParser(
+		description="SplitN_RU - standalone tool that splits the reads with introns (contains 'N' in CIGAR string) in BAM/SAM file into distinct records that doesn't include introns.",
+		epilog="Example:\npython SplitN_RU.py -I <input.bam> -O <output.bam>"
+	)
+
+	# required argument
+	parser.add_argument('-I', action="store", required=True, help='Input alignment file (SAM/BAM).', metavar='<input.bam>')
+
+	# optional arguments
+	parser.add_argument('-O', action="store", help='Output file name.', default='output.bam', metavar='<output.bam>')
+
+	arguments = vars(parser.parse_args())
+	return arguments
+# ----------------------------------------------------------------------
+
 
 def consuming(record):
     ref_consume = 0
     seq_consume = 0
-    cigar_lst = record.get_cigar_stats()
+    cigar_lst = [i for i in record.cigar]
     # M I D N S H P = X B (NM)
-    ref_lst = [0, 2, 3, 4, 7, 8]
+    ref_lst = [0, 2, 3, 7, 8]
     seq_lst = [0, 1, 4, 7, 8]
     for tup in cigar_lst:
         if tup[0] in ref_lst:
-        	ref_consume += 1
+        	ref_consume += tup[1]
         if tup[0] in seq_lst:
-        	seq_consume += 1
+        	seq_consume += tup[1]
     return [ref_consume, seq_consume]
     
 
-# Loading the file
-bam = pysam.AlignmentFile("sorted_with_introns_SRR2973277.bam", 'rb')
-output = pysam.AlignmentFile("output.bam", 'wb', template=bam)
+def main():
+    # Loading the file
+    bam = pysam.AlignmentFile(args['I'], 'rb') #"sorted_with_introns_SRR2973277.bam"
+    output = pysam.AlignmentFile(args['O'], 'wb', template=bam) #"output.bam"
 
-# Loop over the records to process contigs
-for record in bam:
-    # initialize
-    seq = record.seq
-    qual = record.qual
-    original_cigar = record.cigarstring
-    #length = record.query_length
-    ref_pos = record.pos
-    
-    # splitting
-    contigs = record.cigarstring.split('N') # contig by CIGAR
-    contig_start = 0
+    # Loop over the records to process contigs
+    for record in bam:
+        # initialize
+        seq = record.seq
+        qual = record.qual
+        original_cigar = record.cigarstring
+        ref_pos = record.pos
 
-    for contig in contigs:
-    	# prepare
-    	if contig[-1].isdigit():
-    		contig = contig + 'N'
-    	
-    	record.cigarstring = contig
-    	ref_consume, contig_len = consuming(record)
-    	#ref_consume = sum(int(i) for i in re.split(r'[^\d]', contig) if i.isdigit())
-    	
-    	#if contig[-1] == 'D':
-    	#	contig = re.sub(r'\d+$','',contig.rstrip('D'))
-    	#contig_len = sum(int(i) for i in re.split(r'[^\d]', contig) if i.isdigit())
-    	
-    	# update
-    	record.seq = seq[contig_start:contig_start+contig_len]
-    	record.qual = qual[contig_start:contig_start+contig_len]
-    	record.pos = ref_pos
-    	#record.cigarstring = contig
-    	record.tags = []
-    	#### tags!!!! (NM: edit distance), (MD: String for mismatching positions), (NH: Number of reported alignments that contain the query in the current record), (MC: CIGAR string for mate/next segment)
-#    	for tag in record.tags:
-#    		tag = list(tag)
-#    	    if tag[0]=='NM':
-#    	        pass
-#    	    elif tag[0]=='MD':
-#    	        pass
-#    	    elif tag[0]=='NH':
-#    	    	tag[1] += 1
-#    	    tag = tuple(tag)
-    	
-    	# write to the new file
-    	output.write(record)
-    	
-    	# updating variables values
-    	ref_pos += ref_consume
-    	contig_start += contig_len
-    	
-#    break
-    	
-bam.close()
-output.close()
+        # splitting
+        contigs = record.cigarstring.split('N') # contig by CIGAR
+        contig_start = 0
 
-new_bam = pysam.AlignmentFile("output.bam", 'rb')
+        for contig in contigs:
+            # prepare
+            if contig[-1].isdigit():
+                contig = contig + 'N'
+
+            record.cigarstring = contig
+            ref_consume, contig_len = consuming(record)
+
+            if record.cigarstring[-1]=='N':
+                record.cigarstring = re.sub(r'\d+$','',contig.rstrip('N'))
+
+            # update
+            record.seq = seq[contig_start:contig_start+contig_len]
+            record.qual = qual[contig_start:contig_start+contig_len]
+            record.pos = ref_pos
+            if contig_start != 0:
+                record.flag += 2048
+            #record.cigarstring = contig
+            record.tags = []
+            
+            #### tags!!!! (NM: edit distance), (MD: String for mismatching positions), (NH: Number of reported alignments that contain the query in the current record), (MC: CIGAR string for mate/next segment)
+    #    	for tag in record.tags:
+    #    		tag = list(tag)
+    #    	    if tag[0]=='NM':
+    #    	        pass
+    #    	    elif tag[0]=='MD':
+    #    	        pass
+    #    	    elif tag[0]=='NH':
+    #    	    	tag[1] += 1
+    #    	    tag = tuple(tag)
+
+
+            # write to the new file
+            output.write(record)
+
+            # updating variables values
+            ref_pos += ref_consume
+            contig_start += contig_len
+            
+            
+    bam.close()
+    output.close()
+
+
+# ----------------------------------------------------------------------
+if __name__ == '__main__':
+	args = get_args()
+	main()
 
